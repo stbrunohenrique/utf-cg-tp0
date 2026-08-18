96764865ab77bc953de226688ffe15e920de572f
stbrunohenrique <brunohgs06@gmail.com>
2026-08-18 14:15:30 -0300

    JavaScript pronto, mas ideias pendentes...

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("falsegame");
+
+button.addEventListener("click", function() {
+    alert("Não tenho nenhum jogo pra colocar ainda... AINDA!!!");
+    button.style.backgroundColor = "lightblue";
+});
